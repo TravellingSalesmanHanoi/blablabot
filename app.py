7a4ffaa33312e974691d0594313aedb2257bd51b
anon <sotosdragonas@yahoo.com>
2016-12-22 02:23:27 +0200

    Added bs4 to make a quote machine

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import json
+import bs4
 
 import requests
 from flask import Flask, request
 
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET'])
@@ -34,10 +36,17 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
+					
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    try:
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                        words=message_text.split(' ')
+                        if 
+                    except KeyError:
+						send_message(sender_id,'Sorry I can only talk and read in text')
+					
                     
                     
 						
