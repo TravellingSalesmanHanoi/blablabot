a70a77a495712adc4a5e7f687e68dd449d7bf0bc
anon <sotosdragonas@yahoo.com>
2016-12-24 21:40:03 +0200

    Fixed nltk data

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import random
 from textblob import TextBlob
 from textblob import Word
+import nltk
 
 import requests
 from flask import Flask, request
@@ -179,6 +180,8 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 
 if __name__ == '__main__':
   app.run(debug=True)
+ 
+
   
   
   
