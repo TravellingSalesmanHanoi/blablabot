b1ffb5fd1f597c03ec8a995bb93af44c6ebfd054
anon <sotosdragonas@yahoo.com>
2016-12-26 23:12:52 +0200

    Fix for encoding error?

--- app.py
+++ app.py
@@ -69,6 +69,10 @@ def 	goodreads_get(query):
 	
 	return random.choice(quotes)
 	
+
+def gnomiko_get(query):
+	pass
+	
 	
 	
 	
@@ -264,7 +268,7 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 
 if __name__ == '__main__':
   app.run(debug=True)
-  goodreads_get('Love')
+  
   
   
 
